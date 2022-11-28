76619af07d7c77f4514762b9714d7f2ffdabda7f
Zaid <60604829+startswithzed@users.noreply.github.com>
2022-11-28 10:07:26 +0000

    feat(gen-1): add RESTful service to call the core api

---
+++ service.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"errors"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// add a middleware function to log
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.Method, r.RequestURI)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func notAllowedHandler(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "Not Allowed", http.StatusMethodNotAllowed)
+}
+
+func keyValuePutHandler(w http.ResponseWriter, r *http.Request) {
+	// get the key from path variable
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	// read value from request body
+	value, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = Put(key, string(value))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+
+	log.Printf("PUT key=%s value=%s\n", key, string(value))
+}
+
+func keyValueGetHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	value, err := Get(key)
+
+	if errors.Is(err, ErrorNoSuchKey) {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte(value))
+
+	log.Printf("GET key=%s\n", key)
+}
+
+func keyValueDeleteHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	err := Delete(key)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	log.Printf("DELETE key=%s\n", key)
+}
+
+func main() {
+	r := mux.NewRouter()
+
+  // use middleware on the router
+  r.Use(loggingMiddleware)
+
+	// add handlers
+	r.HandleFunc("/v1/{key}", keyValueGetHandler).Methods("GET")
+	r.HandleFunc("/v1/{key}", keyValuePutHandler).Methods("PUT")
+	r.HandleFunc("/v1/{key}", keyValueDeleteHandler).Methods("DELETE")
+
+	// add handlers for other non-handled routes
+	r.HandleFunc("/v1", notAllowedHandler)
+	r.HandleFunc("/v1/{key}", notAllowedHandler)
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
