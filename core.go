ceb0f24e7b40743d3206ed1d39f708a9ee7ac15d
Zaid <60604829+startswithzed@users.noreply.github.com>
2022-12-02 12:35:19 +0000

    docs: add comments

--- core.go
+++ core.go
@@ -5,25 +5,33 @@ import (
 	"sync"
 )
 
+// add a RW mutex to add thread safety
 var store = struct {
 	sync.RWMutex
 	m map[string]string
 }{m: make(map[string]string)}
 
+// creating sentient error
 var ErrorNoSuchKey = errors.New("no such key")
 
 func Delete(key string) error {
+  // establish a write lock 
 	store.Lock()
+  // delete key, value from the map
 	delete(store.m, key)
+  // release the lock
 	store.Unlock()
 
 	return nil
 }
 
 func Get(key string) (string, error) {
-	store.RLock()
+	// establish a read lock
+  store.RLock()
+  // get key, value from the map
 	value, ok := store.m[key]
-	store.RUnlock()
+	// release the lock
+  store.RUnlock()
 
 	if !ok {
 		return "", ErrorNoSuchKey
@@ -33,8 +41,11 @@ func Get(key string) (string, error) {
 }
 
 func Put(key string, value string) error {
+  // establish write lock 
 	store.Lock()
+  // store key value pair in the map
 	store.m[key] = value
+  // release the lock
 	store.Unlock()
 
 	return nil
--- service.go
+++ service.go
@@ -23,85 +23,112 @@ func notAllowedHandler(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, "Not Allowed", http.StatusMethodNotAllowed)
 }
 
+// handles requests to add key value pairs to the database
+// HTTP PUT request handler
 func keyValuePutHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
+	// get key from path variable 
+  vars := mux.Vars(r)
 	key := vars["key"]
 
+  // get value from the request body
 	value, err := ioutil.ReadAll(r.Body)
 	defer r.Body.Close()
+
+  // return status 500 if can't read value
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
+  // add key value pair to the map
 	err = Put(key, string(value))
-	if err != nil {
+	
+  if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
+  // write put event to the log
 	transact.WritePut(key, string(value))
 
+  // return status 201
 	w.WriteHeader(http.StatusCreated)
 
 	log.Printf("PUT key=%s value=%s\n", key, string(value))
 }
 
+// handles requests to get key value pairs from the database
+// HTTP GET request handler
 func keyValueGetHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	key := vars["key"]
 
 	value, err := Get(key)
+
+  // return status 404 if key not found
 	if errors.Is(err, ErrorNoSuchKey) {
 		http.Error(w, err.Error(), http.StatusNotFound)
 		return
 	}
+
+  // return status 500 if any other error
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
+  // write value to response
 	w.Write([]byte(value))
 
 	log.Printf("GET key=%s\n", key)
 }
 
+// handles requests to remove key value pairs from the database
+// HTTP DELETE request handler
 func keyValueDeleteHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	key := vars["key"]
 
 	err := Delete(key)
-	if err != nil {
+	
+  if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
+  // write del event to the log
 	transact.WriteDelete(key)
 
 	log.Printf("DELETE key=%s\n", key)
 }
 
+// initializes the transaction log and runs it
 func initializeTransactionLog() error {
-	var err error
+  var err error
 
 	transact, err = NewTransactionLogger("transactions.log")
+  
 	if err != nil {
 		return fmt.Errorf("failed to create transaction logger: %w", err)
 	}
 
+  // read events from the log 
 	events, errors := transact.ReadEvents()
-	count, ok, e := 0, true, Event{}
+	
+  count, ok, e := 0, true, Event{}
 
+  // this loop runs until either channels are closed or there is an error
 	for ok && err == nil {
 		select {
 		case err, ok = <-errors:
 
+    // execute event based on event type   
 		case e, ok = <-events:
 			switch e.EventType {
-			case EventDelete: // Got a DELETE event!
+			case EventDelete: 
 				err = Delete(e.Key)
 				count++
-			case EventPut: // Got a PUT event!
+			case EventPut: 
 				err = Put(e.Key, e.Value)
 				count++
 			}
@@ -110,20 +137,20 @@ func initializeTransactionLog() error {
 
 	log.Printf("%d events replayed\n", count)
 
+  // run the logger
 	transact.Run()
 
 	return err
 }
 
 func main() {
-	// Initializes the transaction log and loads existing data, if any.
-	// Blocks until all data is read.
+	// blocks until all data is read.
 	err := initializeTransactionLog()
-	if err != nil {
+	
+  if err != nil {
 		panic(err)
 	}
 
-	// Create a new mux router
 	r := mux.NewRouter()
 
 	r.Use(loggingMiddleware)
